66177a6377ab1b8b8ed86b8ec0c5a96bb2eb0b8e
Ashabul Elaph Hilful <hilful25@gmail.com>
2024-12-27 10:37:41 +0600

    Update main.tf

--- lessons/day04/main.tf
+++ lessons/day04/main.tf
@@ -7,7 +7,7 @@ terraform {
   }
   backend "azurerm" {
     resource_group_name  = "tfstate-day04"  # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
-    storage_account_name = "day0417691"                      # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    storage_account_name = "day0420992"                      # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
     container_name       = "tfstate"                       # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
     key                  = "dev.terraform.tfstate"        # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
   }
@@ -18,19 +18,19 @@ provider "azurerm" {
     features {
       
     }
-  
+    subscription_id = "8cbe2d42-5cde-4551-9e4d-dde06c043c64"
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
+resource "azurerm_resource_group" "azday04RG" {
+  name     = "azday04RG"
   location = "West Europe"
 }
 
-resource "azurerm_storage_account" "example" {
+resource "azurerm_storage_account" "azday04RG" {
  
-  name                     = "techtutorial101"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location # implicit dependency
+  name                     = "techtutorial101hilful"
+  resource_group_name      = azurerm_resource_group.azday04RG.name
+  location                 = azurerm_resource_group.azday04RG.location # implicit dependency
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
